d2435ff17af4734bfd8fd64c1198c4ea79f74611
Lian Lu <lianlu@asu.edu>
2016-04-28 23:58:38 -0500

    22

--- lc22.java
+++ lc22.java
@@ -8,5 +8,29 @@ public class Solution {
     List<String> res;
     public List<String> generateParenthesis(int n) {
         res = new ArrayList<String>();
+        helper(new StringBuilder(), n, n);
+        return res;
+    }
+    private void helper(StringBuilder cur, int left, int right){
+      if (left == 0 && right == 0){
+        res.add(cur.toString()); return;
+      }
+      if (left == right){
+        cur.append('(');
+        helper(cur,left - 1, right);
+        cur.remove(cur.length() - 1);
+      }
+      else if (left < right){
+        if (left > 0){
+        cur.append('(');
+        helper(cur,left - 1, right);
+        cur.remove(cur.length() - 1);}
+        if (right > 0){
+        cur.append(')');
+        helper(cur,left, right - 1);
+        cur.remove(cur.length() - 1);
+       }
+      }
+
     }
 }
