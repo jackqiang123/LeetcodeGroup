96ba575afdbc7a9edf50a0eda0518abb7799a2df
Lian Lu <lianlu@asu.edu>
2016-04-29 00:04:10 -0500

    23

---
+++ lc23.java
@@ -0,0 +1,15 @@
+// Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+}
