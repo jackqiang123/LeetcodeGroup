213cb609363b28472c72ca587ced356e91440730
Lian <lianlu@asu.edu>
2016-05-21 16:28:57 -0500

    255v1

--- lc251.java
+++ lc251.java
@@ -8,4 +8,24 @@
 //   [4,5,6]
 // ]
 // By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,2,3,4,5,6].
-public Vector2D(List<List<Integer>> vec2d) {
+public class Solution{
+  int i = 0;
+  int j = 0;
+  List<List<Integer>> vec2d;
+  public Vector2D(List<List<Integer>> vec2d) {
+    i = 0;
+    j = 0;
+    this.vec2d = vec2d;
+  }
+  public int next(){
+    int res = vec2d.get(i).get(j);
+    j++;
+    if (j == vec2d.get(i).size()){
+      i++; j = 0；
+    }
+    return res;
+  }
+  public boolean hasNext(){
+    return i < vec2d.size();
+  }
+}
--- lc255.java
+++ lc255.java
@@ -4,4 +4,23 @@
 //
 // Follow up: Could you do it using only constant space complexity?
 public class Solution {
+
+    // nlogn algrithm
     public boolean verifyPreorder(int[] preorder) {
+      return helper(preorder, 0, preorder.length - 1);
+    }
+    private boolean helper(int []nums, int start, int end){
+      if (start >= end) return true;
+      int rootVal = nums[start];
+      int rightIndex = start;
+      while(rightIndex <= end){
+        if (nums[rightIndex] < rootVal)
+          rightIndex++;
+      }
+      for (int j = rightIndex; j <= end; j++){
+        if (nums[j] < rootVal)
+          return false;
+      }
+      return helper(nums, start + 1, rightIndex - 1) && helper(nums, rightIndex, end);
+    }
+  }
