aa0859920edcd09e3a73067ea18faf27c3badf37
ll <lianlu@asu.edu>
2016-05-24 17:07:49 -0500

    278

--- lc278.java
+++ lc278.java
@@ -1,13 +1,26 @@
-// You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
-//
-// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
-//
-// You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
+// You are a product manager and currently leading a team to develop a new
+// product. Unfortunately, the latest version of your product fails the quality
+// check. Since each version is developed based on the previous version, all the
+// versions after a bad version are also bad. // Suppose you have n versions [1,
+// 2, ..., n] and you want to find out the first bad one, which causes all the
+// following ones to be bad. // You are given an API bool isBadVersion(version)
+// which will return whether version is bad. Implement a function to find the
+// first bad version. You should minimize the number of calls to the API. /* The
+// isBadVersion API is defined in the parent class VersionControl. boolean
+// isBadVersion(int version); */
 
-public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-
+public class Solution extends VersionControl
+{
+  public int firstBadVersion(int n) {
+  int lo =  1;
+  int hi = n;
+  while(lo < hi) {
+    int mid = lo + (hi - lo)/2;
+    if (!isBadVersion(mid)){
+      lo = mid + 1;
     }
+    else hi = mid;
+  }
+  return lo;
+  }
 }
