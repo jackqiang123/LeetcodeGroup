4bbd5cc1277b0119a64aa075ea031c0efa98ae96
ll <lianlu@asu.edu>
2016-05-26 17:08:44 -0500

    300 prev

--- lc272.java
+++ lc272.java
@@ -21,7 +21,85 @@
 //     node using a stack.
 //     You would need two stacks to track the path in finding predecessor and successor node separately.
 public class Solution{
- List<int> closestKValues(TreeNode root, double target, int k) {
+    TreeNode root;
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+      this.root = root;
+      List<Integer> res = new ArrayList<Integer>();
+      TreeNode left = findCrossOver(target);
+      TreeNode right = findSuccessor(left);
+      if (left.val > target){
+          right = cur;
+          left = null;
+      }
+      for (int i = 0; i < k; i++){
+        if (compare(left, right, target))
+        {
+          res.add(left.val);
+          left = getPredecessor(left);
+        }
+        else {
+          res.add(right.val);
+          right = getSuccessor(right);
+        }
+      }
+      return res;
+   }
 
- }
+   private TreeNode findCrossOver(double target){
+     TreeNode cur = root;
+     while(cur != null){
+       if (cur.val == target) return cur;
+       else if (cur.val > target){
+         if (cur.left == null) return cur;
+         else if (cur.left.val < target) return cur.left;
+         else cur = cur.left;
+       }else {
+         if (cur.right == null || cur.right.val > target) return cur;
+         else cur = cur.right;
+       }
+     }
+     return cur;
+   }
+
+   private boolean compare(TreeNode left, TreeNode right, double target){
+     if (right == null) return true;
+     if (left == null) return false;
+     return Math.abs(left.val - target) = Math.abs(right.val - target);
+   }
+
+   private TreeNode getPredecessor(TreeNode node){
+     if (node == null) return null;
+     if (node.left != null){
+       TreeNode res = node.left;
+       while(res.right != null)
+        res = res.right;
+       return res;
+     }
+     else {
+       while(root != null){
+         if (root.right == node) return root;
+         if (root.val < node.val) root = root.right;
+         else root = root.left;
+       }
+     }
+     return null;
+   }
+
+   private TreeNode getSuccessor(TreeNode node){
+     if (node == null) return null;
+     if (node.right != null){
+       TreeNode res = node.right;
+       while(res.left != null)
+        res = res.left;
+       return res;
+     }
+     else {
+       while(root != null){
+         if (root.left == node) return root;
+         if (root.val < node.val) root = root.left;
+         else root = root.right;
+       }
+     }
+     return null;
+   }
  }
---
+++ lc283.java
@@ -0,0 +1,22 @@
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+// For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+//
+// Note:
+//
+//    You must do this in-place without making a copy of the array.
+//    Minimize the total number of operations.
+public class Solution {
+    public void moveZeroes(int[] nums) {
+      int i = 0;
+      int j = 0;
+      while(j < nums.length){
+        if (nums[j] != 0){
+          nums[i++] = nums[j++];
+        }
+        else j++;
+      }
+    }
+    for (; i < nums.length; i++)
+      nums[i] = 0;
+}
